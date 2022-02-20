de30675bdd05e34772897c2839e6dce4db912743
Shpoogtaz <93548948+Shpoogatz@users.noreply.github.com>
2022-02-19 21:27:16 -0500

    Add files via upload

---
+++ Assignment1-1.java
@@ -0,0 +1,11 @@
+// Jacob Creasy
+// January 8, 2022
+// Assignment 1. Greetings
+
+//This is our first assignment of the semester that simply prints out the line..."Good Morning, Professor Dumbledore".
+
+public class Assignment1 {
+    public static void main(String[] args) {
+        System.out.println("Good Morning, Professor Dumbledore");
+    }
+}
